3c85b0b9174b0655496b5ad59d1a297d9c9255e5
Alexey <a.afrosin@tarifer.ru>
2021-01-07 19:01:46 +0400

    настроечный коммит

---
+++ app/src/main/java/com/afrosin/android_part_1/MainActivity.java
@@ -0,0 +1,14 @@
+package com.afrosin.android_part_1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
