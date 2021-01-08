649ace955212795b220425ce5e3d82ca0ea2f0eb
Alexey <a.afrosin@tarifer.ru>
2021-01-08 20:45:35 +0400

    Android 1. Часть 1. Задание 1. Правки по замечаниям

--- app/src/main/java/com/afrosin/android_part_1/MainActivity.java
+++ app/src/main/java/com/afrosin/android_part_1/MainActivity.java
@@ -14,23 +14,23 @@ import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
-    View inc_task_1;
-    View inc_task_2;
-    View inc_task_3;
+    View incTask1;
+    View incTask2;
+    View incTask3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        RadioButton rb_task_1 = findViewById(R.id.rb_task_1);
-        rb_task_1.setOnClickListener(radioButtonClickListener);
+        RadioButton rbTask1 = findViewById(R.id.rb_task_1);
+        rbTask1.setOnClickListener(radioButtonClickListener);
 
-        RadioButton rb_task_2 = findViewById(R.id.rb_task_2);
-        rb_task_2.setOnClickListener(radioButtonClickListener);
+        RadioButton rbTask2 = findViewById(R.id.rb_task_2);
+        rbTask2.setOnClickListener(radioButtonClickListener);
 
-        RadioButton rb_task_3 = findViewById(R.id.rb_task_3);
-        rb_task_3.setOnClickListener(radioButtonClickListener);
+        RadioButton rbTask3 = findViewById(R.id.rb_task_3);
+        rbTask3.setOnClickListener(radioButtonClickListener);
 
         TextView textViewSelectedDate = findViewById(R.id.textViewSelectedDate);
         CalendarView calendar_view = findViewById(R.id.calendarView);
@@ -38,35 +38,35 @@ public class MainActivity extends AppCompatActivity {
 
         DecimalFormat mFormat = new DecimalFormat("00");
 
-        Date tmp_date = new Date(calendar_view.getDate());
-        textViewSelectedDate.setText(String.format(this.getBaseContext().getString(R.string.selected_date), DateFormat.format("dd", tmp_date), DateFormat.format("MM", tmp_date), DateFormat.format("yyyy", tmp_date)));
+        Date tmpDate = new Date(calendar_view.getDate());
+        textViewSelectedDate.setText(String.format(this.getBaseContext().getString(R.string.selected_date), DateFormat.format("dd", tmpDate), DateFormat.format("MM", tmpDate), DateFormat.format("yyyy", tmpDate)));
 
         calendar_view.setOnDateChangeListener((view, year, month, dayOfMonth) ->
                 textViewSelectedDate.setText(String.format(this.getBaseContext().getString(R.string.selected_date), mFormat.format(Double.valueOf(dayOfMonth)), mFormat.format(Double.valueOf(month + 1)), String.valueOf(year))));
 
 
-        inc_task_1 = findViewById(R.id.inc_task_1);
-        inc_task_2 = findViewById(R.id.inc_task_2);
-        inc_task_3 = findViewById(R.id.inc_task_3);
+        incTask1 = findViewById(R.id.inc_task_1);
+        incTask2 = findViewById(R.id.inc_task_2);
+        incTask3 = findViewById(R.id.inc_task_3);
     }
 
     View.OnClickListener radioButtonClickListener = v -> {
         RadioButton rb = (RadioButton) v;
         switch (rb.getId()) {
             case R.id.rb_task_1:
-                inc_task_1.setVisibility(View.VISIBLE);
-                inc_task_2.setVisibility(View.GONE);
-                inc_task_3.setVisibility(View.GONE);
+                incTask1.setVisibility(View.VISIBLE);
+                incTask2.setVisibility(View.GONE);
+                incTask3.setVisibility(View.GONE);
                 break;
             case R.id.rb_task_2:
-                inc_task_1.setVisibility(View.GONE);
-                inc_task_2.setVisibility(View.VISIBLE);
-                inc_task_3.setVisibility(View.GONE);
+                incTask1.setVisibility(View.GONE);
+                incTask2.setVisibility(View.VISIBLE);
+                incTask3.setVisibility(View.GONE);
                 break;
             case R.id.rb_task_3:
-                inc_task_1.setVisibility(View.GONE);
-                inc_task_2.setVisibility(View.GONE);
-                inc_task_3.setVisibility(View.VISIBLE);
+                incTask1.setVisibility(View.GONE);
+                incTask2.setVisibility(View.GONE);
+                incTask3.setVisibility(View.VISIBLE);
                 break;
         }
     };
