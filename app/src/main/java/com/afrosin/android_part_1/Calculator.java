43bf90cbc778d42802838520834e357fd43f957a
AlexeyAfrosin <68603357+AlexeyAfrosin@users.noreply.github.com>
2021-01-18 18:07:35 +0400

    Android 1. Часть 1. Задание 3 (#3)
    * Android 1. Часть 1. Задание 3.

---
+++ app/src/main/java/com/afrosin/android_part_1/Calculator.java
@@ -0,0 +1,178 @@
+package com.afrosin.android_part_1;
+
+
+import java.io.Serializable;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Calculator implements Serializable {
+
+    private final String OPERATION_PERCENT = "%";
+    private final String OPERATION_DIVISION = "/";
+    private final String OPERATION_MULTIPLICATION = "*";
+    private final String OPERATION_MINUS = "-";
+    private final String OPERATION_ADD = "+";
+    private final String OPERATION_DIGIT_SEPARATOR = ".";
+    private final String OPERATION_DIG_0 = "0";
+    private final String OPERATION_DIG_1 = "1";
+    private final String OPERATION_DIG_2 = "2";
+    private final String OPERATION_DIG_3 = "3";
+    private final String OPERATION_DIG_4 = "4";
+    private final String OPERATION_DIG_5 = "5";
+    private final String OPERATION_DIG_6 = "6";
+    private final String OPERATION_DIG_7 = "7";
+    private final String OPERATION_DIG_8 = "8";
+    private final String OPERATION_DIG_9 = "9";
+
+    private final String DIGIT_PATTERN = "(\\d+(?:\\.\\d+)?)";
+    //([-+*\/%])
+    private final String OPERATION_PATTERN = String.format("([%s%s%s\\%s%s])", OPERATION_MINUS, OPERATION_ADD, OPERATION_MULTIPLICATION, OPERATION_DIVISION, OPERATION_PERCENT);
+
+    private double calcResult;
+    private StringBuilder displayString;
+    private StringBuilder stringForCalc;
+
+    public Calculator() {
+        this.calcResult = 0;
+        this.displayString = new StringBuilder();
+        this.stringForCalc = new StringBuilder();
+    }
+
+    public double getCalcResult() {
+        return calcResult;
+    }
+
+    public String getCurrentOperationAsString() {
+        return displayString.toString();
+    }
+
+    public void setCurrentOperation(String str, CalcOperation operation) {
+
+        switch (operation) {
+            case AC:
+                clearData();
+                break;
+            case DEL:
+                deleteLastChar();
+                break;
+            case ADD:
+                appendStr(str, OPERATION_ADD);
+                break;
+            case MINUS:
+                appendStr(str, OPERATION_MINUS);
+                break;
+            case DIVISION:
+                appendStr(str, OPERATION_DIVISION);
+                break;
+            case MULTIPLICATION:
+                appendStr(str, OPERATION_MULTIPLICATION);
+                break;
+            case PERCENT:
+                appendStr(str, OPERATION_PERCENT);
+                break;
+            case DIGIT_SEPARATOR:
+                appendStr(str, OPERATION_DIGIT_SEPARATOR);
+                break;
+            case DIG_0:
+                appendStr(str, OPERATION_DIG_0);
+                break;
+            case DIG_1:
+                appendStr(str, OPERATION_DIG_1);
+                break;
+            case DIG_2:
+                appendStr(str, OPERATION_DIG_2);
+                break;
+            case DIG_3:
+                appendStr(str, OPERATION_DIG_3);
+                break;
+            case DIG_4:
+                appendStr(str, OPERATION_DIG_4);
+                break;
+            case DIG_5:
+                appendStr(str, OPERATION_DIG_5);
+                break;
+            case DIG_6:
+                appendStr(str, OPERATION_DIG_6);
+                break;
+            case DIG_7:
+                appendStr(str, OPERATION_DIG_7);
+                break;
+            case DIG_8:
+                appendStr(str, OPERATION_DIG_8);
+                break;
+            case DIG_9:
+                appendStr(str, OPERATION_DIG_9);
+                break;
+            case EQUALLY:
+                calcResult();
+                break;
+            default:
+                appendStr(str, str);
+        }
+    }
+
+    private void appendStr(String displayStr, String forCalcStr) {
+        this.displayString.append(displayStr);
+        this.stringForCalc.append(forCalcStr);
+    }
+
+    private void clearData() {
+        this.displayString.setLength(0);
+        this.stringForCalc.setLength(0);
+    }
+
+    private void calcResult() {
+
+        String str = stringForCalc.toString();
+        calcResult = 0;
+
+        if (str.replaceAll(String.format("\\%s", OPERATION_DIGIT_SEPARATOR), "").length() > 0) {
+
+            double tmpDigit;
+
+            Pattern pattern = Pattern.compile(OPERATION_PATTERN);
+            Matcher operation_matcher = pattern.matcher(str);
+
+            pattern = Pattern.compile(DIGIT_PATTERN);
+            Matcher digit_matcher = pattern.matcher(str);
+
+            digit_matcher.find();
+            calcResult = strToDouble(digit_matcher.group(0));
+
+            while (digit_matcher.find()) {
+                tmpDigit = strToDouble(digit_matcher.group(0));
+                operation_matcher.find();
+
+                switch (operation_matcher.group(0)) {
+                    case OPERATION_ADD:
+                        calcResult += tmpDigit;
+                        break;
+                    case OPERATION_MINUS:
+                        calcResult -= tmpDigit;
+                        break;
+                    case OPERATION_DIVISION:
+                        calcResult /= tmpDigit;
+                        break;
+                    case OPERATION_MULTIPLICATION:
+                        calcResult *= tmpDigit;
+                        break;
+//                case OPERATION_PERCENT:
+//                    calcResult *= tmpDigit / 100;
+//                    break;
+                }
+            }
+        }
+    }
+
+    public double strToDouble(String str) {
+        return Double.parseDouble(str);
+    }
+
+    public void deleteLastChar() {
+
+        if (displayString.length() > 0) {
+            displayString.setLength(displayString.length() - 1);
+            stringForCalc.setLength(stringForCalc.length() - 1);
+        }
+    }
+}
--- app/src/main/java/com/afrosin/android_part_1/MainActivity.java
+++ app/src/main/java/com/afrosin/android_part_1/MainActivity.java
@@ -1,14 +1,181 @@
 package com.afrosin.android_part_1;
 
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import java.text.DecimalFormatSymbols;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private TextView twCurrentOperation;
+    private TextView twCalcResult;
+    private Calculator calculator;
+    private final static String KEY_CALCULATOR = "calculator";
+    private final static String KEY_CURRENT_OPERATION = "current_operation";
+    private final static String KEY_RESULT_OPERATION = "result_operation";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        calculator = new Calculator();
+        twCurrentOperation = findViewById(R.id.tw_current_operation);
+        twCalcResult = findViewById(R.id.tw_calc_result);
+        String[] operationButtons = {"bt_ac", "bt_del", "bt_percent", "bt_division", "bt_multiplication",
+                "bt_minus", "bt_add", "bt_equally", "bt_digit_separator"
+        };
+
+        for (String operationButton : operationButtons) {
+            Button btTmp = findViewById(getResources().getIdentifier(operationButton, "id", getPackageName()));
+            btTmp.setOnClickListener(this);
+        }
+
+        for (int i = 0; i <= 9; i++) {
+            Button btTmp = findViewById(getResources().getIdentifier(String.format("bt_dig_%d", i), "id", getPackageName()));
+            btTmp.setOnClickListener(this);
+        }
     }
+
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle instanceState) {
+        super.onSaveInstanceState(instanceState);
+        instanceState.putSerializable(KEY_CALCULATOR, calculator);
+        instanceState.putString(KEY_CURRENT_OPERATION, twCurrentOperation.getText().toString());
+        instanceState.putString(KEY_RESULT_OPERATION, twCalcResult.getText().toString());
+    }
+
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle instanceState) {
+        super.onRestoreInstanceState(instanceState);
+        calculator = (Calculator) instanceState.getSerializable(KEY_CALCULATOR);
+        twCalcResult.setText(instanceState.getString(KEY_RESULT_OPERATION));
+        twCurrentOperation.setText(instanceState.getString(KEY_CURRENT_OPERATION));
+    }
+
+    @Override
+    public void onClick(View view) {
+        int btnId = view.getId();
+
+        switch (btnId) {
+            case R.id.bt_ac:
+                calcAction(twCurrentOperation, R.string.bt_ac, CalcOperation.AC);
+                break;
+            case R.id.bt_del:
+                calcAction(twCurrentOperation, R.string.bt_del, CalcOperation.DEL);
+                break;
+            case R.id.bt_percent:
+                calcAction(twCurrentOperation, R.string.bt_percent, CalcOperation.PERCENT);
+                break;
+            case R.id.bt_division:
+                calcAction(twCurrentOperation, R.string.bt_division, CalcOperation.DIVISION);
+                break;
+            case R.id.bt_multiplication:
+                calcAction(twCurrentOperation, R.string.bt_multiplication, CalcOperation.MULTIPLICATION);
+                break;
+            case R.id.bt_minus:
+                calcAction(twCurrentOperation, R.string.bt_minus, CalcOperation.MINUS);
+                break;
+            case R.id.bt_add:
+                calcAction(twCurrentOperation, R.string.bt_add, CalcOperation.ADD);
+                break;
+            case R.id.bt_equally:
+                calcAction(twCurrentOperation, R.string.bt_equally, CalcOperation.EQUALLY);
+                showResult();
+                break;
+            case R.id.bt_digit_separator:
+                calcAction(twCurrentOperation, R.string.bt_digit_separator, CalcOperation.DIGIT_SEPARATOR);
+                break;
+            case R.id.bt_dig_0:
+                calcAction(twCurrentOperation, R.string.dig_0, CalcOperation.DIG_0);
+                break;
+            case R.id.bt_dig_1:
+                calcAction(twCurrentOperation, R.string.dig_1, CalcOperation.DIG_1);
+                break;
+            case R.id.bt_dig_2:
+                calcAction(twCurrentOperation, R.string.dig_2, CalcOperation.DIG_2);
+                break;
+            case R.id.bt_dig_3:
+                calcAction(twCurrentOperation, R.string.dig_3, CalcOperation.DIG_3);
+                break;
+            case R.id.bt_dig_4:
+                calcAction(twCurrentOperation, R.string.dig_4, CalcOperation.DIG_4);
+                break;
+            case R.id.bt_dig_5:
+                calcAction(twCurrentOperation, R.string.dig_5, CalcOperation.DIG_5);
+                break;
+            case R.id.bt_dig_6:
+                calcAction(twCurrentOperation, R.string.dig_6, CalcOperation.DIG_6);
+                break;
+            case R.id.bt_dig_7:
+                calcAction(twCurrentOperation, R.string.dig_7, CalcOperation.DIG_7);
+                break;
+            case R.id.bt_dig_8:
+                calcAction(twCurrentOperation, R.string.dig_8, CalcOperation.DIG_8);
+                break;
+            case R.id.bt_dig_9:
+                calcAction(twCurrentOperation, R.string.dig_9, CalcOperation.DIG_9);
+                break;
+        }
+    }
+
+    private void showResult() {
+
+        String strCurrentOperation = twCurrentOperation.getText().toString();
+
+        if (strCurrentOperation.replaceAll(String.format("\\%s", getStringById(this, R.string.bt_digit_separator)), "").length() > 0) {
+            double result = calculator.getCalcResult();
+
+            if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT) {
+                twCalcResult.append(String.format(getDigitFormat(result, twCalcResult.getText().toString()), strCurrentOperation, getStringById(this, R.string.bt_equally), result));
+            } else {
+                String oldStr = twCalcResult.getText().toString();
+                twCalcResult.setText("");
+                twCalcResult.append(String.format(getDigitFormat(result, oldStr), strCurrentOperation, getStringById(this, R.string.bt_equally), result));
+                twCalcResult.append("\n");
+                twCalcResult.append(oldStr);
+            }
+        }
+
+        // очищаем
+        calcAction(twCurrentOperation, R.string.bt_ac, CalcOperation.AC);
+    }
+
+    // получаем формат выводимой строки
+    private String getDigitFormat(double calcResult, String textViewText) {
+        StringBuilder strFormat = new StringBuilder();
+
+        String strResult = String.format("%f", calcResult);
+
+        if (textViewText.length() > 0) {
+            strFormat.append("\n");
+        }
+        strFormat.append("%s\n%s\n");
+        // вычисляем количество нулей после запятой
+        if (strResult.indexOf(DecimalFormatSymbols.getInstance().getDecimalSeparator()) > 0) {
+            int decimalSeparatorIdx = strResult.indexOf(DecimalFormatSymbols.getInstance().getDecimalSeparator());
+            String tmpStr = strResult.substring(decimalSeparatorIdx + 1);
+            int countDigitAfterSeparator = tmpStr.replaceAll("(0{1,}$)", "").length();
+            strFormat.append(String.format("%s%s%d%s", "%", ".", countDigitAfterSeparator, "f"));
+        } else {
+            strFormat.append("%.0f");
+        }
+        return strFormat.toString();
+    }
+
+    private void calcAction(TextView twCurrentOperation, int stringId, CalcOperation operation) {
+        calculator.setCurrentOperation(getStringById(this, stringId), operation);
+        twCurrentOperation.setText(calculator.getCurrentOperationAsString());
+    }
+
+    private static String getStringById(MainActivity activity, int resourceId) {
+        return activity.getBaseContext().getString(resourceId);
+    }
+
 }
